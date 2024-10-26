3fe41c5a2367c4f84c3a872ec9c57c6251e49c9d
Admin <Admin@LAPTOP-DH3TSSHI>
2024-10-26 14:19:48 +0300

    Add fixed plugins

---
+++ PhonePluginsApplication_fixed2/EmployeesLoaderPlugin/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoneApp.Domain;
+using PhoneApp.Domain.Attributes;
+using PhoneApp.Domain.DTO;
+using PhoneApp.Domain.Interfaces;
+
+namespace EmployeesLoaderPlugin
+{ 
+
+  class myclass
+  {
+    public string Name { get; set; }
+    public string Phone { get; set; }
+  }
+  [Author(Name = "Ivan Petrov")]
+  public class Plugin : IPluggable
+  {
+    private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+    public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
+    {
+      logger.Info("Loading employees");
+      var employeesList = args.Cast<EmployeesDTO>().ToList();
+      var tmp = Newtonsoft.Json.JsonConvert.DeserializeObject<List<myclass>>(EmployeesLoaderPlugin.Properties.Resources.EmployeesJson);
+      for (int i = 0; i < tmp.Count; ++i)
+      {
+        EmployeesDTO e = new EmployeesDTO();
+        e.Name = tmp[i].Name;
+        e.AddPhone(tmp[i].Phone);
+        employeesList.Add(e);
+      }
+      logger.Info($"Loaded {employeesList.Count()} employees");
+      return employeesList.Cast<DataTransferObject>();
+    }
+  }
+}
